87788a13574b1dda5c03d2c35a5bfa583c6ca0f1
alexicum <alshestakov25@gmail.com>
2016-06-21 15:41:03 +0300

    Handle response with status 400 and text instead of json
    1. package.json updated to have only necessary dependencies
    2. Handle response with status 400 and text instead of json
    3. index.android.js could be copied into index.ios.js for use with iOS.

--- index.android.js
+++ index.android.js
@@ -1,51 +1,242 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+import React from 'react';
+import ReactNative from 'react-native';
+import * as t from 'tcomb-form-native';
 
-import React, { Component } from 'react';
-import {
+var {
   AppRegistry,
+  AsyncStorage,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  TouchableHighlight,
+  Alert,
+} = ReactNative;
 
-class AwesomeProject extends Component {
+var STORAGE_KEY = 'id_token';
+
+var Form = t.form.Form;
+
+/*
+ For Windows:
+ for Genymotion + VirtualBox use:
+ http://10.0.3.2
+ Discussed here: http://stackoverflow.com/questions/18463319/access-host-from-genymotion-emulator
+ or
+ Ethernet adapter VirtualBox Host-Only Network
+ IPv4 address, for me: http://192.168.56.1
+ for AVD: http://10.0.2.2
+ not tested.
+ */
+const MY_IP = 'http://10.0.3.2:3001';
+
+var Person = t.struct({
+  username: t.String,
+  password: t.String
+});
+
+const options = {};
+
+var ReactNativeAuth = React.createClass({
+
+  async _onValueChange(item, selectedValue) {
+    try {
+      await AsyncStorage.setItem(item, selectedValue);
+    } catch (error) {
+      console.log('AsyncStorage error: ' + error.message);
+    }
+  },
+
+  async _getProtectedQuote() {
+    var DEMO_TOKEN = await AsyncStorage.getItem(STORAGE_KEY);
+    fetch(MY_IP + "/api/protected/random-quote", {
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + DEMO_TOKEN
+      }
+    })
+      .then(this.handleRequestErrors)
+      .then((response) => response.text())
+      .then((quote) => {
+        Alert.alert(
+          "Chuck Norris Quote:", quote,)
+      })
+      .done(
+        undefined,
+        (error) => {
+          console.log('_getProtectedQuote.done.reject.' + error.message);
+          Alert.alert(
+            "Get quote Error!",
+            'There has been a problem with your fetch operation: ' + error.message);
+        }
+      );
+  },
+
+  async _userLogout() {
+    try {
+      await AsyncStorage.removeItem(STORAGE_KEY);
+      Alert.alert("Logout Success!")
+    } catch (error) {
+      console.log('AsyncStorage error: ' + error.message);
+    }
+  },
+
+  async handleRequestErrors(response) {
+    if (!response.ok) {
+      if (response.statusText)
+        throw new Error(response.statusText);
+      return await response.text().then(
+        (text) => {
+          console.log('Error: ' + text);
+          throw new Error(text);
+        }
+      );
+    }
+    return response;
+  },
+
+  _userSignup() {
+    var value = this.refs.form.getValue();
+    if (value) { // if validation fails, value will be null
+      fetch(MY_IP + "/users", {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json, text/html',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: value.username,
+          password: value.password
+        })
+      })
+        .then(this.handleRequestErrors)
+        .then((response) => {
+          console.log('return response.json();');
+          return response.json();
+        })
+        .then((responseData) => {
+          console.log('Signup. then((responseData) => .');
+          this._onValueChange(STORAGE_KEY, responseData.id_token);
+        })
+        .done(
+          () => {
+            console.log('Signup.done.resolve.');
+            Alert.alert(
+              "Signup Success!",
+              "Click the button to get a Chuck Norris quote!"
+            )
+          },
+          (error) => {
+            console.log('Signup.done.reject.' + error.message);
+            Alert.alert(
+              "Signup Error!",
+              'There has been a problem with your fetch operation: ' + error.message);
+          }
+        );
+    }
+  },
+
+  _userLogin() {
+    var value = this.refs.form.getValue();
+    if (value) { // if validation fails, value will be null
+      fetch(MY_IP + "/sessions/create", {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: value.username,
+          password: value.password
+        })
+      })
+        .then(this.handleRequestErrors)
+        .then((response) => {
+          console.log('Login. response.json().');
+          return response.json();
+        })
+        .then((responseData) => {
+          console.log('Login. then((responseData) =>.');
+          Alert.alert(
+            "Login Success!",
+            "Click the button to get a Chuck Norris quote!"
+          );
+          this._onValueChange(STORAGE_KEY, responseData.id_token)
+        })
+        .done(
+          () => {
+            console.log('Login.done.resolve.');
+          },
+          (error) => {
+            console.log('Login.done.reject.' + error.message);
+            Alert.alert(
+              "Login  Error!",
+              'There has been a problem with your fetch operation: ' + error.message);
+          }
+        );
+    }
+  },
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={styles.row}>
+          <Text style={styles.title}>Signup/Login below for Chuck Norris Quotes!</Text>
+        </View>
+        <View style={styles.row}>
+          <Form
+            ref="form"
+            type={Person}
+            options={options}
+          />
+        </View>
+        <View style={styles.row}>
+          <TouchableHighlight style={styles.button} onPress={this._userSignup} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Signup</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button} onPress={this._userLogin} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button} onPress={this._userLogout} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Logout</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.row}>
+          <TouchableHighlight onPress={this._getProtectedQuote} style={styles.button}>
+            <Text style={styles.buttonText}>Get a Chuck Norris Quote!</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
-}
+});
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
   },
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('ReactNativeAuth', () => ReactNativeAuth);
